efbd230a980c42e430982479151eea6617a4acd8
rwadada <wadada.knct@gmail.com>
2019-10-13 23:45:41 +0900

    set alert when opened youtube

--- background.js
+++ background.js
@@ -1,30 +1,9 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-        code: `
-            var bgElement = document.createElement('div');
-            var s = bgElement.style;
-            s.position = 'fixed';
-            s.left = '-100%';
-            var title = document.title;
-            var url = window.location.href;
-            bgElement.appendChild(document.createElement('pre')).textContent = title;
-            bgElement.appendChild(document.createElement('pre')).textContent = url;
-            document.body.appendChild(bgElement);
-            document.getSelection().selectAllChildren(bgElement);
-            var result = document.execCommand('copy');
-            document.body.removeChild(bgElement);
-        `
+window.onload = function () {
+    chrome.tabs.getSelected(window.id, function (tab) {
+        //tab.urlに開いているタブのURLが入っている
+        var url = document.createTextNode(tab.url);
+        if (url.startWith("https://youtube.com/")) {
+            window.alert("仕事しろ！")
+        }
     });
-
-    chrome.browserAction.setBadgeText({
-        tabId: tab.id,
-        text: 'Copy'
-    });
-    setTimeout(function(){
-        chrome.browserAction.setBadgeText({
-            tabId: tab.id,
-            text: ''
-        });
-    }, 1000);
-
-});
+}
